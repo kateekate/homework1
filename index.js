30f2446e4c694ced8d647e953754ea3e3129cfad
Katya <katya.mikhaylova.98@list.ru>
2023-08-01 23:30:33 +0200

    add: complete second task

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Task 1
+
 function convertNums() {
   const num = +prompt('Введите первое число');
   const base = +prompt('Введите второе число');
@@ -11,3 +13,26 @@ function convertNums() {
 }
 
 convertNums();
+
+// Task 2
+
+function calculateNums() {
+  const num1 = +prompt('Введите первое число');
+
+  if (Number.isNaN(num1)) {
+    console.log('Некорректный ввод!');
+    return;
+  }
+
+  const num2 = +prompt('Введите второе число');
+
+  if (!Number.isNaN(num2)) {
+    const sum = num1 + num2;
+    const quotient = num1 / num2;
+    console.log(`Ответ: ${sum}, ${quotient}`);
+  } else {
+    console.log('Некорректный ввод!');
+  }
+}
+
+calculateNums();
