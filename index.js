21367c430e4944ac547bcf5e5dba1515f745ad4c
Katya <katya.mikhaylova.98@list.ru>
2023-08-02 16:36:24 +0200

    refactor: improve code readability

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ function convertNums() {
   const num = +prompt('Введите первое число');
   const base = +prompt('Введите второе число');
 
-  if (!Number.isNaN(num) && !Number.isNaN(base) && base >= 2 && base <= 36) {
-    const res = num.toString(base);
-    console.log(res);
+  const isValidNums = !isNaN(num) && !isNaN(base);
+  const isValidBase = base >= 2 && base <= 36;
+
+  if (isValidNums && isValidBase) {
+    console.log(num.toString(base));
   } else {
     console.log('Некорректный ввод!');
   }
@@ -19,19 +21,16 @@ convertNums();
 function calculateNums() {
   const num1 = +prompt('Введите первое число');
 
-  if (Number.isNaN(num1)) {
-    console.log('Некорректный ввод!');
-    return;
+  if (isNaN(num1)) {
+    return console.log('Некорректный ввод!');
   }
 
   const num2 = +prompt('Введите второе число');
 
-  if (!Number.isNaN(num2)) {
-    const sum = num1 + num2;
-    const quotient = num1 / num2;
-    console.log(`Ответ: ${sum}, ${quotient}`);
-  } else {
+  if (isNaN(num2)) {
     console.log('Некорректный ввод!');
+  } else {
+    console.log(`Ответ: ${num1 + num2}, ${num1 / num2}`);
   }
 }
 
